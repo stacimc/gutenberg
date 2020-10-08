390e69f916ab827d57d9c6b75d85be7caff8f157
Jeremy Yip <jeremy.yip@automattic.com>
2020-10-07 17:08:14 -0700

    Refactor navigation sidebar directory structure
    Because navigation panel and navigation toggle live outside the header and body, we place both components in their own navigation sidebar directory

--- packages/edit-site/src/components/left-sidebar/index.js
+++ packages/edit-site/src/components/left-sidebar/index.js
@@ -3,12 +3,7 @@
  */
 import InserterPanel from './inserter-panel';
 
-// TODO: Remove remnants of conditional rendering for nav sidebar
 const LeftSidebar = ( { content, setContent } ) => {
-	// if ( content === 'navigation' ) {
-	// 	return <NavigationPanel />;
-	// }
-
 	if ( content === 'inserter' ) {
 		return <InserterPanel closeInserter={ () => setContent( null ) } />;
 	}
